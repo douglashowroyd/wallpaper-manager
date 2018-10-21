92a60b3b11170fe4d4fdfcae65a4c80df24d72a9
douglashowroyd <36495176+douglashowroyd@users.noreply.github.com>
2018-10-21 21:11:58 +0100

    Initial commits
    Initial upload. ML still needs to be written. Folder_Sorter and Wallpaper_Manager should be working.

---
+++ Folder_Sorter.py
@@ -0,0 +1,224 @@
+'''
+Need to run this on first set up. Or another file calling this one.
+
+Can't assume cd'ed into wallpaper folder either
+'''
+
+
+
+
+import ctypes
+import os
+import numpy as np
+import pandas as pd
+import shutil
+
+
+
+
+def Move_Into_Temp():
+    '''Makes an empty temp folder, 
+            moves the old temp folder to a new one if such a folder exists 
+            then moves all folders into temp folder'''
+    folder_dir = os.listdir()
+    if 'Temp' in folder_dir:
+        new_temp='Temp'
+        count = 0
+        while new_temp in folder_dir:
+            count += 1
+            new_temp = 'Temp-%d' % (count,) 
+        os.mkdir(new_temp)
+        folder_dir.append(new_temp)
+        folder_dir.remove('Temp')
+        for file in os.listdir('Temp'):
+            shutil.move(os.path.join('Temp',file),os.path.join(new_temp,file))
+            print('Moving %s from Temp folder to' % (file,), new_temp)
+    else:
+        os.mkdir('Temp')
+    
+    # move all files in path to temp folder
+    for file in folder_dir:
+        shutil.move(file, os.path.join('Temp',file))
+        print('Moving %s to Temp folder' % (file,))
+
+    
+    
+
+
+def Temp_To_New():
+    '''Move all files to New folder from temp folder'''
+    
+    print('Creating New folder')
+    os.mkdir('New')
+    print('Creating Used folder')
+    os.mkdir('Used')
+    print('Creating Errors folder')    
+    os.mkdir('Errors')
+
+
+    # take list of all files and directories in temp
+    folder_dir = os.listdir('Temp')
+    
+    while len(folder_dir)>0:
+    
+        root, dirs, files = next(os.walk('Temp'))
+        
+        
+        # first deal with loose files
+        for file in files:
+            head = file.split(".")[0]
+            tail = file.split(".")[-1]
+    
+     
+            # if not a picture move to error file and we will deal with it later
+            if tail not in ['pjpeg', 'gif', 'bmp','jpg','jpeg','png']:
+                #move to main folder first to avoid bad interactions
+                shutil.move(os.path.join('Temp',file),file)
+                dst_file = os.path.join('Errors', file)
+                tail = '.' + tail
+                # now check no name overlaps
+                count = 0
+                while os.path.exists(dst_file):
+                    count += 1
+                    dst_file = os.path.join('Errors', '%s-%d%s' % (head, count, tail))
+                print('Renaming %s to %s' % (file, dst_file))
+                shutil.move(file, dst_file)
+                continue
+
+    
+    
+            # now check no name overlaps then move to New folder   
+            shutil.move(os.path.join('Temp',file),file)
+            
+            dst_file = os.path.join('New', file)
+            tail = '.' + tail
+            # rename if necessary
+            count = 0
+            while os.path.exists(dst_file):
+                count += 1
+                dst_file = os.path.join('New', '%s-%d%s' % (head, count, tail))
+            print('Renaming %s to %s' % (file, dst_file))
+            shutil.move(file, dst_file)     
+
+    
+      
+        # takes all objects in all folders in temp and moves them into temp themselves, then deletes the empty folders
+        for folder_name in dirs:
+            second_path = os.path.join('Temp', folder_name)
+            
+            second_root, second_dirs, second_files = next(os.walk(second_path))
+            
+            # move files from folder_name to temp, renaming where needed
+            for file in second_files:
+                head = file.split(".")[0]
+                tail = '.' + file.split(".")[-1]
+                
+                # now check no name overlaps
+                shutil.move(os.path.join('Temp',folder_name,file), file)
+                dst_file = os.path.join('Temp', file)
+                count = 0
+                while os.path.exists(dst_file):
+                    count += 1
+                    dst_file = os.path.join('Temp', '%s-%d%s' % (head, count, tail))
+                print('Renaming %s to %s' % (file, dst_file))
+                shutil.move(file, dst_file)
+                
+                
+                
+            # move folders to temp folder
+            for folder in second_dirs:
+                # first move to main folder
+                if folder == 'New':
+                    shutil.move(os.path.join('Temp',folder_name,folder),'New-1')
+                    folder = 'New-1'
+                elif folder == 'Temp':
+                    shutil.move(os.path.join('Temp',folder_name,folder),'temp-1')
+                    folder = 'Temp-1'
+                elif folder == 'Used':
+                    shutil.move(os.path.join('temp',folder_name,folder),'Used-1')
+                    folder = 'Used-1'
+                elif folder == 'Errors':
+                    shutil.move(os.path.join('Temp',folder_name,folder),'Errors-1')
+                    folder = 'Errors-1'
+                else:
+                    shutil.move(os.path.join('Temp',folder_name,folder),folder)
+                
+                # now check no name overlaps
+                dst_dir = os.path.join('Temp', folder)
+                count = 0
+                while os.path.exists(dst_dir):
+                    count += 1
+                    dst_dir = os.path.join('Temp', '%s-%d' % (folder, count))
+                print('Renaming %s to %s' % (folder, dst_dir))
+                shutil.move(folder, dst_dir)
+                
+                
+            os.rmdir(os.path.join('Temp',folder_name))
+            
+        # recheck temp folder
+        folder_dir = os.listdir('Temp')
+        
+    
+    shutil.rmtree('Temp')
+     
+
+
+
+        
+
+    
+    
+def DataFrame_Populate(path):
+    '''start data frame of all images'''
+    data = []
+    df = pd.DataFrame(data, columns = ['Names','Extension','Kept','Rejected','Total Score','New or Used', 'Weight'])
+    
+    
+    newdir_list = os.listdir('New')
+    
+    
+    for file in newdir_list:
+        head = file.split(".")[0]
+        tail = file.split(".")[-1]
+    
+    
+        # add file and etension to dataframe
+        df2 = pd.DataFrame([[head,tail,0,0,0,0,1]], columns=['Names','Extension','Kept','Rejected','Total Score','Weight'])
+        df=df.append(df2, ignore_index=True, sort = True)
+    
+
+    cols = ['Names','Extension','Kept','Rejected','Total Score','New or Used','Weight']
+    df = df[cols]
+    print(df.head())
+    df.to_csv(os.path.join(path,'wallpaper_data.csv'))
+    print('A list of all your wallpapers can be found in your wallpaper folder as a .csv file')
+        
+        
+    # need to analyse each pic at the end for rgb values
+
+    
+    
+    
+    
+    
+    
+def Check_Errors_Folder():
+    '''Informs user about the number of errors or delete folder if empty'''
+    no_of_errors = len(os.listdir('Errors'))
+    if  no_of_errors == 1:
+        print("There was 1 incompatible file.")
+        print("It can be found in", os.path.join(path,'Errors'))
+        print("If you would like to use this file as a wallpaper, please convert it to a valid format and rerun this program.")
+
+    elif no_of_errors > 1:
+        print("There were ", no_of_errors," incompatible files.")
+        print("They can be found in", os.path.join(path,'Errors'))
+        print("If you would like to use any of those files as wallpapers, please convert them to a valid format and rerun this program.")
+    else:
+        shutil.rmtree('Errors')
+
+        
+        
+        
+        
+        
---
+++ Wallpaper_Manager.py
@@ -0,0 +1,138 @@
+'''To Do:
+This will go in main program folder as exe. (need to find where specs.txt has to sit to be useable)
+
+Need to set this to run on startup.
+
+Need to add machine learning stuff in ML file then call func here.
+
+Need to check it works again
+'''
+    
+
+import ctypes
+import os
+import random
+import tkinter as tk
+
+
+
+
+# This picks a random picture in your specified folder + New, makes it the current wallpaper then 
+#      moves it to the used folder
+def Random_Wallpaper():
+    with open('specs.txt', 'r') as file:
+        specs = file.readlines()
+    main_path = specs[0]
+    old_wallpaper = specs[1]
+    
+    # Load possible wallpapers and weights 
+    df = pd.read_csv("wallpaper_data.csv")
+    wallpapers = df.loc[df['New or Used'] == 0]
+    wallpapers_list = df.loc[:, 'Names']
+    weights = df.loc[:,'Weight']
+
+    # Pick one with a suitable weight
+    new_random_wallpaper = np.random.choice(wallpapers_list, 1, p=weights)
+    new_wallpaper_path = os.path.join(main_path,'New',new_random_wallpaper[0])
+    
+    # Change Wallpaper
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, new_wallpaper_path, 3)
+
+    # Update data for that wallpaper
+    df.loc[df['Names'] == old_wallpaper, 'Rejected'] += 1
+    df.loc[df['Names'] == new_random_wallpaper[0], 'New or Used'] = 1
+
+    # Move wallpaper to Used folder and check if New folder is empty
+    Move_Wallpaper(new_random_wallpaper[0])
+    Check_empty_folder()
+    
+    # Save current wallpaper to text file for future reference and add 1 to count
+    with open('specs.txt', 'r') as file:
+        specs = file.readlines()
+        
+    specs[1] = new_random_wallpaper[0]
+    specs[2] += 1
+    
+    with open('specs.txt', 'w') as file:
+        file.writelines( specs )
+    
+    
+    
+    
+# This just moves the file from the New folder to the Used folder
+def Move_Wallpaper(wallpaper):
+    old_name=os.path.join("New",wallpaper)
+    new_name=os.path.join("Used",wallpaper)
+    os.rename(old_name,new_name)
+
+    
+    
+# This checks if the New folder is empty and if so moves all the used images back into the New folder and updates data
+def Check_empty_folder():
+    if len(os.listdir('New')) == 0:
+        os.rename('New', 'New(1)')
+        os.rename('Used', 'New')
+        os.rename('New(1)', 'Used')
+        df = pd.read_csv("wallpaper_data.csv")
+        df.loc[:, 'New or Used'] = 0
+        
+        
+        
+# This just updates the current wallpapers kept count
+def Keep_Wallpaper():
+    df = pd.read_csv("wallpaper_data.csv")
+    with open('specs.txt', 'r') as file:
+        specs = file.readlines()
+    df.loc[df['Names'] == specs[1], 'Kept'] += 1
+       
+    
+    
+class Change_Wallpaper(tk.Frame):
+
+    def __init__(self, master):
+        self.master = master
+        tk.Frame.__init__(self, self.master)
+        self.configure_gui()
+        self.create_widgets()
+        self.pack()
+
+    def configure_gui(self):
+        self.master.title("Wallpaper Picker")
+        self.master.geometry("300x75")
+        self.master.resizable(False, False)
+        
+    def create_widgets(self):
+        self.message= tk.Label(self, text="Are you happy with  this wallpaper?")
+        self.message.pack(side="top", fill="x")
+        
+        self.yes= tk.Button(self, text="Yes", command=self.keep)
+        self.yes.pack(side="left")
+        
+        self.space=tk.Label(self)
+        self.space.pack(side="left")
+        
+        self.no= tk.Button(self, text="No",command=self.change)
+        self.no.pack(side="right")        
+        
+    def change(self):
+        Random_Wallpaper()
+        
+    def keep(self):
+        Keep_Wallpaper()
+        root.destroy
+        
+        
+        
+if __name__ == '__main__':
+    # Need to change this so user path is one from specs file and use chdir
+    with open('specs.txt', 'r') as file:
+        specs = file.readlines()
+    main_path = specs[0]
+    os.chdir(main_path)
+    
+    root = tk.Tk()
+    change_wallpaper = Change_Wallpaper(root)
+    root.mainloop()
+    
+    
+    # Need to do ML stuff here
